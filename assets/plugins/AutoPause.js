d448055ba051954eb338d9d1b8d191b6c7bd47fa
josehmaza <josehmaza@gmail.com>
2019-10-20 17:44:50 -0500

    VisibilityChange

--- assets/plugins/AutoPause.js
+++ assets/plugins/AutoPause.js
@@ -2,9 +2,11 @@ class AutoPause {
     constructor(){
         this.threshold = 0.25
         this.handlerIntersection = this.handlerIntersection.bind(this)
+        this.handlerVisibilityChange = this.handlerVisibilityChange.bind(this)
+        document.addEventListener('visibilitychange', this.handlerVisibilityChange)
+        
     }
     run(player){
-        debugger
         this.player = player
         const observer = new IntersectionObserver(this.handlerIntersection, {
             threshold: this.threshold  // 25%
@@ -12,9 +14,19 @@ class AutoPause {
         observer.observe(this.player.media)
     }
     handlerIntersection(entries){
+        //hay que hacer bin to this
         const entry = entries[0]
         const esVisible = entry.intersectionRatio >= this.threshold
         if(esVisible){
+            this.player.play()
+        }else{
+            this.player.pause()
+        }
+    }
+    handlerVisibilityChange(){
+        //Hay que hacer bind a this
+        const isVisible = document.visibilityState === 'visible'
+        if(isVisible){
             this.player.play()
         }else{
             this.player.pause()
